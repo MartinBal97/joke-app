171198cda3993a957a40c943cc410749f2f0282b
Martin Balverde <tinchoabalverde@gmail.com>
2023-02-08 11:03:59 +0100

    Joke challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const [btn, setBtn] = useState(true);
+  const [joke, setJoke] = useState({})
+
+  useEffect(() => {
+    fetch('https://icanhazdadjoke.com/', {
+      headers: {
+        "Accept": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setJoke(data));
+  }, [btn])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='containerJoke'>
+        <h3>Don't Laugh Challenge</h3>
+        <p>{joke.joke}</p>
+        <button onClick={() => setBtn(!btn)}>Get Another Joke</button>
+      </div>
+    </>
+  )
 }
 
-export default App;
